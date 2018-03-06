08e4ac588bee8d581479205345d3792fa1a75d7e
Will McCarty <wpm1414@gmail.com>
2018-03-06 12:52:40 -0600

    added problem description

--- arraysum.py
+++ arraysum.py
@@ -1,5 +1,10 @@
-# Given an array and an input k, calculate if any two elements sum to k 
+#Given an array of numbers, return whether any two sums to K.
+#For example, given [10, 15, 3, 7] and K of 17, return true since 10 + 7 is 17.
+
 # Solution is O(n^2) with some trimming of impossible solutions 
+# Other solutions
+# 	sort and then some sort of binary search within a range of valid answers to avoid the linear scan 
+# 		This would give O(nlgn) 
 
 #input: 
 # @param array of positive integers 
